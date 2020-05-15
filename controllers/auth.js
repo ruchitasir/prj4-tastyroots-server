0ff68d7b630e236694deb35a611c77c6d776c37b
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-05-15 09:13:51 -0700

    Signup route created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 let db = require('../models')
+let jwt = require('jsonwebtoken')
 let router = require('express').Router()
 
 // POST /auth/login (find and validate user; send token)
@@ -11,7 +12,38 @@ router.post('/login', (req, res) => {
 // POST to /auth/signup (create user; generate token)
 router.post('/signup', (req, res) => {
   console.log(req.body)
-  res.send('STUB POST /auth/signup')
+  // Look up the User by email to make sure that the User is new
+  db.User.findOne({email: req.body.email})
+  .then(user=>{
+    // If a user exists already then do NOT let them create another account
+    if(user){
+      // No no! Sign up
+      return res.status(409).send({message: 'Email address in use'})
+    }
+     //We know the user is legitimately a new user so create new user account
+      db.User.create(req.body)
+      .then(newUser=>{
+        // User exists, so create a token for the new user
+        let token = jwt.sign(newUser.toJSON(),process.env.JWT_SECRET,{
+            expiresIn: 120 // 60 * 60* 8 // 8 hours, in seconds
+        })
+        res.send({token})
+      })
+      .catch(innerErr=>{
+        console.log("Error creating a new user",innerErr)
+        if(innerErr.name=== 'ValidationError'){
+          res.status(412).send({message: `Validation Error! ${innerErr.message}.`})
+        }else{
+          res.status(500).send({message: 'Error creating user'})
+        }
+        
+      })
+  })
+  .catch(err=>{
+    console.log("Error in post /auth/signup when checking if the user already exists,err")
+    res.status(503).send({message: 'Database or server error'})
+  })
+
 })
 
 // NOTE: User should be logged in to access this route
--- models/user.js
+++ models/user.js
@@ -1,5 +1,59 @@
+let bcrypt= require('bcryptjs')
 let mongoose = require('mongoose')
 
-// TODO: Create user schema
+//  Create user schema
+let userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: String,
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 6
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    pic: String,
+    admin:{
+        type: Boolean,
+        default: false
+    }
 
-// TODO: Export user model
+})
+
+  // Hash the password with Bcrypt
+  userSchema.pre('save',function(done){
+        // Make sure it's new, as opppsed to modified
+        if(this.isNew){ 
+            this.password = bcrypt.hashSync(this.password,12)
+        }
+        // Indicate that we are okay to move on (to insert into the DB)
+        done()
+  })  
+
+
+// Make a JSON representation of the user (for sending on the JWT payload)
+  userSchema.set('toJSON',{
+      transform: (doc,user)=>{
+            delete user.password
+            delete user.__v
+            return user
+      }
+  })
+
+
+// Make a function that compares passwords
+userSchema.methods.validPassword= function (typedPassword) {
+    // typedPassword: Plain text, just typed in by user
+    // this.password: Existing, hashed password
+    return bcrypt.compareSync(typedPassword, this.password)
+}
+
+// Export user model
+module.exports = mongoose.model('User',userSchema)
